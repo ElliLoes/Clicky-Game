96bbcf4a70d17ea03aeb64c5c4d30bf2e5b87040
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-12-04 16:17:17 +1100

    Added image file

---
+++ src/images.js
@@ -0,0 +1,16 @@
+import donut1 from "./assets/images/donut1.png";
+import donut2 from "./assets/images/donut2.jpg";
+import donut3 from "./assets/images/donut3.jpg";
+import donut4 from "./assets/images/donut4.jpg";
+import donut5 from "./assets/images/donut5.jpg";
+import donut6 from "./assets/images/donut6.jpg";
+import donut7 from "./assets/images/donut7.jpg";
+import donut8 from "./assets/images/donut8.jpg";
+import donut9 from "./assets/images/donut9.jpg";
+import donut10 from "./assets/images/donut10.jpg";
+import donut11 from "./assets/images/donut11.jpg";
+import donut12 from "./assets/images/donut12.jpg";
+
+const images = [donut1, donut2, donut3, donut4, donut5, donut6, donut7, donut8, donut9, donut10, donut11, donut12];
+  
+export default images;
