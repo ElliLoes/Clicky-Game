903e4bbf4d42b6938e441a34a31598c9ed1e7808
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-12-07 21:37:22 +1100

    Updates

---
+++ precache-manifest.159556b164322517d8d0a0c059009db6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c099d969f562eb3515c7756ef5825905",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0d4a57cac58ae6792144",
+    "url": "/Clicky-Game/static/css/main.b2236f3d.chunk.css"
+  },
+  {
+    "revision": "e281f53b345671cd6c08",
+    "url": "/Clicky-Game/static/js/2.fa921bb9.chunk.js"
+  },
+  {
+    "revision": "0d4a57cac58ae6792144",
+    "url": "/Clicky-Game/static/js/main.d1119f87.chunk.js"
+  },
+  {
+    "revision": "e040d85a65e1489afcab",
+    "url": "/Clicky-Game/static/js/runtime-main.ccb7d40c.js"
+  },
+  {
+    "revision": "d5059a9bffb349ac67012f2879f6f1ac",
+    "url": "/Clicky-Game/static/media/donut1.d5059a9b.png"
+  },
+  {
+    "revision": "7d70ed0e325945f62d1db6334347f43e",
+    "url": "/Clicky-Game/static/media/donut10.7d70ed0e.jpg"
+  },
+  {
+    "revision": "5098cb3818c7f166c58f6c39a0f1dfce",
+    "url": "/Clicky-Game/static/media/donut11.5098cb38.jpg"
+  },
+  {
+    "revision": "8ec318a61007927cd5d8c959da3ff9a5",
+    "url": "/Clicky-Game/static/media/donut12.8ec318a6.jpg"
+  },
+  {
+    "revision": "ee31c4c6577cb0799d01dc6693d4d65c",
+    "url": "/Clicky-Game/static/media/donut2.ee31c4c6.jpg"
+  },
+  {
+    "revision": "632c8dbb0e2a1a319eafd4cf09f6b475",
+    "url": "/Clicky-Game/static/media/donut3.632c8dbb.jpg"
+  },
+  {
+    "revision": "436acdf5ef64ccaae24704f914daa4dd",
+    "url": "/Clicky-Game/static/media/donut4.436acdf5.jpg"
+  },
+  {
+    "revision": "5be7fac633ffbf4faafc9e71b6b30b8d",
+    "url": "/Clicky-Game/static/media/donut5.5be7fac6.jpg"
+  },
+  {
+    "revision": "0e9f482e8dfcffdd1e600520638daa70",
+    "url": "/Clicky-Game/static/media/donut6.0e9f482e.jpg"
+  },
+  {
+    "revision": "2514b054423c3dded860e578c2330633",
+    "url": "/Clicky-Game/static/media/donut7.2514b054.jpg"
+  },
+  {
+    "revision": "a694651045f8e55f0b75435caea064e7",
+    "url": "/Clicky-Game/static/media/donut8.a6946510.jpg"
+  },
+  {
+    "revision": "0cc1e425cc8de4251b34bde3a2d616bc",
+    "url": "/Clicky-Game/static/media/donut9.0cc1e425.jpg"
+  }
+]);
